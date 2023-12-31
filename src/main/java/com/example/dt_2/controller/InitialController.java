9daaf1572090fd3e37dc5d78d9f39fa12e846f8a
David Kirowski <david.kiro@sgv.es>
2023-12-31 21:57:05 +0100

    updated InitialController

--- src/main/java/com/example/dt_2/controller/InitialController.java
+++ src/main/java/com/example/dt_2/controller/InitialController.java
@@ -1,15 +1,18 @@
 package com.example.dt_2.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/test")
 public class InitialController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String helloWorld() {
-        return "Hello World!";
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from unsecured endpoint");
     }
 }
